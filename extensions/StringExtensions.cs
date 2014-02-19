c72063c9201669bb0bc2cb472d5c8e11da564f91
Ryan Christensen <ryan@drawlabs.com>
2014-02-18 18:36:56 -0700

    Update StringExtensions.cs

--- extensions/StringExtensions.cs
+++ extensions/StringExtensions.cs
@@ -149,47 +149,4 @@ public static class StringExtensions {
         return Cryptos.HashMD5(val);
     }
 
-    public static string ToJSON(this string val, object obj) {
-        return JsonConvert.SerializeObject(obj).FilterJSON();
-    }
-
-    public static T FromJSON<T>(this string val) {
-        if (!string.IsNullOrEmpty(val)) {
-            try {
-                return JsonConvert.DeserializeObject<T>(val.FilterJSON());
-            }
-            catch (Exception e) {
-                Web.Log("FromJSON:e:", e);
-                return default(T);
-            }
-        }
-        else {
-            return default(T);
-        }
-    }
-    public static string FilterJSON(this string val) {
-        if (string.IsNullOrEmpty(val))
-            return val;
-        return val.Replace("\\\"", "\"").TrimStart('"').TrimEnd('"');
-    }
-
-    public static object ConvertJSON(this string val) {
-        if (val.StartsWith("{") || val.StartsWith("[")) {
-            try {
-
-                if (val.TrimStart().StartsWith("{")) {
-                    return val.FilterJSON().FromJSON<Dictionary<string, object>>();                
-                }
-                else if (val.TrimStart().StartsWith("[")) {
-                    return val.FilterJSON().FromJSON<List<object>>();   
-                }
-
-            }
-            catch (Exception e) {
-                Web.Log("ERROR parsing attribute:", e);
-            }
-        }
-
-        return val;
-    }
 }
