30406afd529508eb2b0fccb550bbca38af513fd0
Ryan Christensen <ryan@drawlabs.com>
2014-02-18 18:36:26 -0700

    Update ObjectExtensions.cs

--- extensions/ObjectExtensions.cs
+++ extensions/ObjectExtensions.cs
@@ -5,6 +5,8 @@ using System.Web;
 
 using Newtonsoft.Json;
 
+using Labs.Utility;
+
 public static class ObjectExtensions {
 
     public static string ToJSON(this object val) {
@@ -15,4 +17,43 @@ public static class ObjectExtensions {
         return JsonConvert.SerializeObject(val, indented ? Formatting.Indented : Formatting.None).FilterJSON();
     }
 
+    public static T FromJSON<T>(this string val) {
+        if (!string.IsNullOrEmpty(val)) {
+            try {
+                return JsonConvert.DeserializeObject<T>(val.FilterJSON());
+            }
+            catch (Exception e) {
+                Web.Log("FromJSON:e:", e);
+                return default(T);
+            }
+        }
+        else {
+            return default(T);
+        }
+    }
+    public static string FilterJSON(this string val) {
+        if (string.IsNullOrEmpty(val))
+            return val;
+        return val.Replace("\\\"", "\"").TrimStart('"').TrimEnd('"');
+    }
+
+    public static object ConvertJSON(this string val) {
+        if (val.StartsWith("{") || val.StartsWith("[")) {
+            try {
+
+                if (val.TrimStart().StartsWith("{")) {
+                    return val.FilterJSON().FromJSON<Dictionary<string, object>>();
+                }
+                else if (val.TrimStart().StartsWith("[")) {
+                    return val.FilterJSON().FromJSON<List<object>>();
+                }
+
+            }
+            catch (Exception e) {
+                Web.Log("ERROR parsing attribute:", e);
+            }
+        }
+
+        return val;
+    }
 }
